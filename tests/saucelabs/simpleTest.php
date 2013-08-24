d7599be23bd9aacf6e1df4d1f17c2effe091b097
cioddi <maxtobiasweber@gmail.com>
2013-08-24 21:00:05 +0200

    initial

--- tests/saucelabs/simpleTest.php
+++ tests/saucelabs/simpleTest.php
@@ -17,100 +17,100 @@ class simpleDemoTest extends Sauce\Sausage\WebDriverTestCase
                 'platform' => 'OS X 10.8'
             )
         )
-        // ,array(
-        //     'browserName' => 'ipad',
-        //     'desiredCapabilities' => array(
-        //         'version' => '5.1',
-        //         'platform' => 'OS X 10.8'
-        //     )
-        // ),
-        // array(
-        //     'browserName' => 'firefox',
-        //     'desiredCapabilities' => array(
-        //         'version' => '',
-        //         'platform' => 'Windows 8'
-        //     )
-        // ),
-        // array(
-        //     'browserName' => 'chrome',
-        //     'desiredCapabilities' => array(
-        //         'version' => '',
-        //         'platform' => 'Windows 8'
-        //     )
-        // ),array(
-        //     'browserName' => 'android',
-        //     'desiredCapabilities' => array(
-        //         'version' => '4.0',
-        //         'platform' => 'Linux'
-        //     )
-        // ),array(
-        //     'browserName' => 'internet explorer',
-        //     'desiredCapabilities' => array(
-        //         'version' => '10',
-        //         'platform' => 'Windows 8'
-        //     )
-        // ),array(
-        //     'browserName' => 'internet explorer',
-        //     'desiredCapabilities' => array(
-        //         'version' => '9',
-        //         'platform' => 'Windows 7'
-        //     )
-        // ),array(
-        //     'browserName' => 'opera',
-        //     'desiredCapabilities' => array(
-        //         'version' => '',
-        //         'platform' => 'Windows 7'
-        //     )
-        // ),
-        // array(
-        //     'browserName' => 'firefox',
-        //     'desiredCapabilities' => array(
-        //         'version' => '',
-        //         'platform' => 'Windows 7'
-        //     )
-        // ),
-        // array(
-        //     'browserName' => 'chrome',
-        //     'desiredCapabilities' => array(
-        //         'version' => '',
-        //         'platform' => 'Windows 7'
-        //     )
-        // ),
-        // array(
-        //     'browserName' => 'chrome',
-        //     'desiredCapabilities' => array(
-        //         'version' => '',
-        //         'platform' => 'Linux'
-        //     )
-        // ),
-        // array(
-        //     'browserName' => 'firefox',
-        //     'desiredCapabilities' => array(
-        //         'version' => '',
-        //         'platform' => 'Linux'
-        //     )
-        // ),
-        // array(
-        //     'browserName' => 'chrome',
-        //     'desiredCapabilities' => array(
-        //         'version' => '',
-        //         'platform' => 'OS X 10.8'
-        //     )
-        // ),
-        // array(
-        //     'browserName' => 'safari',
-        //     'desiredCapabilities' => array(
-        //         'version' => '6',
-        //         'platform' => 'OS X 10.8'
-        //     )
-        // ),
-        // array(
-        //     'browserName' => 'firefox',
-        //     'desiredCapabilities' => array(
-        //         'version' => '',
-        //         'platform' => 'OS X 10.6'
-        //     )
-        // )
+        ,array(
+            'browserName' => 'ipad',
+            'desiredCapabilities' => array(
+                'version' => '5.1',
+                'platform' => 'OS X 10.8'
+            )
+        ),
+        array(
+            'browserName' => 'firefox',
+            'desiredCapabilities' => array(
+                'version' => '',
+                'platform' => 'Windows 8'
+            )
+        ),
+        array(
+            'browserName' => 'chrome',
+            'desiredCapabilities' => array(
+                'version' => '',
+                'platform' => 'Windows 8'
+            )
+        ),array(
+            'browserName' => 'android',
+            'desiredCapabilities' => array(
+                'version' => '4.0',
+                'platform' => 'Linux'
+            )
+        ),array(
+            'browserName' => 'internet explorer',
+            'desiredCapabilities' => array(
+                'version' => '10',
+                'platform' => 'Windows 8'
+            )
+        ),array(
+            'browserName' => 'internet explorer',
+            'desiredCapabilities' => array(
+                'version' => '9',
+                'platform' => 'Windows 7'
+            )
+        ),array(
+            'browserName' => 'opera',
+            'desiredCapabilities' => array(
+                'version' => '',
+                'platform' => 'Windows 7'
+            )
+        ),
+        array(
+            'browserName' => 'firefox',
+            'desiredCapabilities' => array(
+                'version' => '',
+                'platform' => 'Windows 7'
+            )
+        ),
+        array(
+            'browserName' => 'chrome',
+            'desiredCapabilities' => array(
+                'version' => '',
+                'platform' => 'Windows 7'
+            )
+        ),
+        array(
+            'browserName' => 'chrome',
+            'desiredCapabilities' => array(
+                'version' => '',
+                'platform' => 'Linux'
+            )
+        ),
+        array(
+            'browserName' => 'firefox',
+            'desiredCapabilities' => array(
+                'version' => '',
+                'platform' => 'Linux'
+            )
+        ),
+        array(
+            'browserName' => 'chrome',
+            'desiredCapabilities' => array(
+                'version' => '',
+                'platform' => 'OS X 10.8'
+            )
+        ),
+        array(
+            'browserName' => 'safari',
+            'desiredCapabilities' => array(
+                'version' => '6',
+                'platform' => 'OS X 10.8'
+            )
+        ),
+        array(
+            'browserName' => 'firefox',
+            'desiredCapabilities' => array(
+                'version' => '',
+                'platform' => 'OS X 10.6'
+            )
+        )
     );
     
 
